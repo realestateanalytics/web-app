33a7df68499ebd21fa723392ce79443d2189c6d6
Konstantin Zabashta <kosta.zabashta@gmail.com>
2016-07-28 16:51:23 -0400

    Changed the url

--- app/js/services/property/property.js
+++ app/js/services/property/property.js
@@ -1,7 +1,6 @@
 module.exports = function($http) {
   var getPropertyDetails = function(mls_id) {
-    console.log('http://localhost:3005/api/listing_detail/' + mls_id);
-    return $http.get('http://localhost:3005/api/listing_detail/' + mls_id)
+    return $http.get('http://re-web-api.azurewebsites.net/api/listing_detail/' + mls_id)
       .then(
         function (response) {
           return response.data;
